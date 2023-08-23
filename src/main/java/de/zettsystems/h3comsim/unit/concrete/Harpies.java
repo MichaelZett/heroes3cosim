f3d52f1c1528c263c9dbb376a8e218a0bc4684a6
Michael Zöller <michael2.zoeller@gmail.com>
2023-08-23 11:39:10 +0200

    Add more units

---
+++ src/main/java/de/zettsystems/h3comsim/unit/concrete/Harpies.java
@@ -0,0 +1,81 @@
+package de.zettsystems.h3comsim.unit.concrete;
+
+import de.zettsystems.h3comsim.unit.common.AbstractUnit;
+import de.zettsystems.h3comsim.unit.common.AttackType;
+import de.zettsystems.h3comsim.unit.common.Movement;
+
+import java.util.Collections;
+
+public class Harpies extends AbstractUnit {
+
+    private final static String NAME = "Harpies";
+    private final static int ATTACK = 6;
+    private final static int DEFENSE = 5;
+    private final static int HEALTH = 14;
+    private final static int SPEED = 6;
+    private final static int MIN_DAMAGE = 1;
+    private final static int MAX_DAMAGE = 4;
+    private final static Movement MOVEMENT = Movement.FLYING;
+    private final static int SHOTS = 0;
+    private final static int COST = 130;
+    private final static AttackType ATTACK_TYPE = AttackType.HAND_TO_HAND;
+
+    public Harpies() {
+        super(HEALTH, Collections.emptySet());
+    }
+
+    @Override
+    public String getName() {
+        return NAME;
+    }
+
+    @Override
+    public int getAttack() {
+        return ATTACK;
+    }
+
+    @Override
+    public int getDefense() {
+        return DEFENSE;
+    }
+
+    @Override
+    public int getHealth() {
+        return HEALTH;
+    }
+
+    @Override
+    public int getSpeed() {
+        return SPEED;
+    }
+
+    @Override
+    public int getMinDamage() {
+        return MIN_DAMAGE;
+    }
+
+    @Override
+    public int getMaxDamage() {
+        return MAX_DAMAGE;
+    }
+
+    @Override
+    public Movement getMovement() {
+        return MOVEMENT;
+    }
+
+    @Override
+    public int getShots() {
+        return SHOTS;
+    }
+
+    @Override
+    public int getCost() {
+        return COST;
+    }
+
+    @Override
+    public AttackType getAttackType() {
+        return ATTACK_TYPE;
+    }
+}
---
+++ src/main/java/de/zettsystems/h3comsim/unit/concrete/InfernalTroglodyte.java
@@ -0,0 +1,81 @@
+package de.zettsystems.h3comsim.unit.concrete;
+
+import de.zettsystems.h3comsim.unit.common.AbstractUnit;
+import de.zettsystems.h3comsim.unit.common.AttackType;
+import de.zettsystems.h3comsim.unit.common.Movement;
+
+import java.util.Collections;
+
+public class InfernalTroglodyte extends AbstractUnit {
+
+    private final static String NAME = "Infernal Troglodyte";
+    private final static int ATTACK = 5;
+    private final static int DEFENSE = 4;
+    private final static int HEALTH = 6;
+    private final static int SPEED = 5;
+    private final static int MIN_DAMAGE = 1;
+    private final static int MAX_DAMAGE = 3;
+    private final static Movement MOVEMENT = Movement.GROUND;
+    private final static int SHOTS = 0;
+    private final static int COST = 65;
+    private final static AttackType ATTACK_TYPE = AttackType.HAND_TO_HAND;
+
+    public InfernalTroglodyte() {
+        super(HEALTH, Collections.emptySet());
+    }
+
+    @Override
+    public String getName() {
+        return NAME;
+    }
+
+    @Override
+    public int getAttack() {
+        return ATTACK;
+    }
+
+    @Override
+    public int getDefense() {
+        return DEFENSE;
+    }
+
+    @Override
+    public int getHealth() {
+        return HEALTH;
+    }
+
+    @Override
+    public int getSpeed() {
+        return SPEED;
+    }
+
+    @Override
+    public int getMinDamage() {
+        return MIN_DAMAGE;
+    }
+
+    @Override
+    public int getMaxDamage() {
+        return MAX_DAMAGE;
+    }
+
+    @Override
+    public Movement getMovement() {
+        return MOVEMENT;
+    }
+
+    @Override
+    public int getShots() {
+        return SHOTS;
+    }
+
+    @Override
+    public int getCost() {
+        return COST;
+    }
+
+    @Override
+    public AttackType getAttackType() {
+        return ATTACK_TYPE;
+    }
+}
